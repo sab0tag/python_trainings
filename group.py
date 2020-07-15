29fab616b47c350dd4fd31e1c665a34120b5a459
Ihor Petrenko <sabotag1985@gmail.com>
2020-07-15 13:42:32 +0300

    Параметры, которые передаются в методе create_group сгруппированы и вынесен в отдельный класс

---
+++ group.py
@@ -0,0 +1,5 @@
+class Group:
+    def __init__(self, groupName, headerDescr, footerDescr):
+        self.groupName = groupName
+        self.headerDescr = headerDescr
+        self.footerDescr = footerDescr
