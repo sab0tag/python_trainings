2ab12e60dd1ef43a4d34d699595b1490077aa152
Ihor Petrenko <sabotag1985@gmail.com>
2020-07-15 17:56:12 +0300

    add_contact.py
    Добавлен файл usr.py
    Параметры, которые передаются в методе add_usr сгруппированы и вынесен в отдельный класс

--- add_group.py
+++ add_group.py
@@ -6,7 +6,47 @@ import unittest, time as t
 from group import Group
 
 
-class AppDynamicsJob(unittest.TestCase):
+def open_home_page(driver):
+    driver.get("http://localhost:8080/addressbook/index.php")
+
+
+def login(driver, user, pwd):
+    driver.find_element_by_name("user").click()
+    driver.find_element_by_name("user").clear()
+    driver.find_element_by_name("user").send_keys(user)
+    driver.find_element_by_name("pass").clear()
+    driver.find_element_by_name("pass").send_keys(pwd)
+    driver.find_element_by_xpath("//input[@value='Login']").click()
+
+
+def open_groups_page(driver):
+    driver.find_element_by_id("container").click()
+    driver.find_element_by_link_text("groups").click()
+
+
+def create_group(driver, group):
+    driver.find_element_by_name("new").click()
+    driver.find_element_by_name("group_name").click()
+    driver.find_element_by_name("group_name").clear()
+    # fill group form
+    driver.find_element_by_name("group_name").send_keys(group.groupName)
+    driver.find_element_by_name("group_header").clear()
+    driver.find_element_by_name("group_header").send_keys(group.headerDescr)
+    driver.find_element_by_name("group_footer").clear()
+    driver.find_element_by_name("group_footer").send_keys(group.footerDescr)
+    # submit group creation
+    driver.find_element_by_name("submit").click()
+
+
+def return_to_groups_page(driver):
+    driver.find_element_by_link_text("group page").click()
+
+
+def logout(driver):
+    driver.find_element_by_link_text("Logout").click()
+
+
+class add_group(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(30)
@@ -14,60 +54,25 @@ class AppDynamicsJob(unittest.TestCase):
         self.verificationErrors = []
         self.accept_next_alert = True
 
-    def open_home_page(self, driver):
-        # open homepage
-        driver.get("http://localhost:8080/addressbook/index.php")
-
-    def login(self, driver, user, pwd):
-        driver.find_element_by_name("user").click()
-        driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys(user)
-        driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys(pwd)
-        driver.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_groups_page(self, driver):
-        driver.find_element_by_id("container").click()
-        driver.find_element_by_link_text("groups").click()
-
-    def create_group(self, driver, group):
-        driver.find_element_by_name("new").click()
-        driver.find_element_by_name("group_name").click()
-        driver.find_element_by_name("group_name").clear()
-        # fill group form
-        driver.find_element_by_name("group_name").send_keys(group.groupName)
-        driver.find_element_by_name("group_header").clear()
-        driver.find_element_by_name("group_header").send_keys(group.headerDescr)
-        driver.find_element_by_name("group_footer").clear()
-        driver.find_element_by_name("group_footer").send_keys(group.footerDescr)
-        # submit group creation
-        driver.find_element_by_name("submit").click()
-
-    def return_to_groups_page(self, driver):
-        driver.find_element_by_link_text("group page").click()
-
-    def logout(self, driver):
-        driver.find_element_by_link_text("Logout").click()
-
     def test_add_new_group(self):
         driver = self.driver
-        self.open_home_page(driver)
+        open_home_page(driver)
         t.sleep(2)
-        self.login(driver, user="admin", pwd="secret")
-        self.open_groups_page(driver)
-        self.create_group(driver, Group(groupName="newGroup", headerDescr="header description", footerDescr="foter description"))
-        self.return_to_groups_page(driver)
-        self.logout(driver)
+        login(driver, user="admin", pwd="secret")
+        open_groups_page(driver)
+        create_group(driver, Group(groupName="newGroup", headerDescr="header description", footerDescr="footer description"))
+        return_to_groups_page(driver)
+        logout(driver)
 
     def test_empty_group(self):
         driver = self.driver
-        self.open_home_page(driver)
+        open_home_page(driver)
         t.sleep(2)
-        self.login(driver, user="admin", pwd="secret")
-        self.open_groups_page(driver)
-        self.create_group(driver, Group(groupName="", headerDescr="", footerDescr=""))
-        self.return_to_groups_page(driver)
-        self.logout(driver)
+        login(driver, user="admin", pwd="secret")
+        open_groups_page(driver)
+        create_group(driver, Group(groupName="", headerDescr="", footerDescr=""))
+        return_to_groups_page(driver)
+        logout(driver)
 
     def is_element_present(self, how, what):
         try:
---
+++ usr.py
@@ -0,0 +1,16 @@
+class Users:
+    def __init__(self, name, surname, nick, titl, company_name, street, mobile_number, email_1, email_2, b_day,
+                 b_month, b_year, street2):
+        self.name = name
+        self.surname = surname
+        self.nick = nick
+        self.titl = titl
+        self.company_name = company_name
+        self.street = street
+        self.mobile_number = mobile_number
+        self.email_1 = email_1
+        self.email_2 = email_2
+        self.b_day = b_day
+        self.b_month = b_month
+        self.b_year = b_year
+        self.street2 = street2
