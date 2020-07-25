937e563f8696c346798df468abd90b1fd6ef2fb8
Ihor Petrenko <sabotag1985@gmail.com>
2020-07-25 09:20:58 +0300

    - из тестовых методов событие login перенесено в метод, который инициализирует фикстуру -  добавлена проверка инициализации фикстуры с использованием кострукции if/then/else - в application.py добавлен метод is_valid, проверяющий событие "Открытие браузера" и возможность считать ссылку, который мы используем чтобы проинициализировать фикстуру - для разрушения фикстуры определен отдельный метод

--- fixture/application.py
+++ fixture/application.py
@@ -15,6 +15,13 @@ class Application:
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
 
+    def is_valid(self):
+        try:
+            self.driver.current_url
+            return True
+        except:
+            return False
+
     def open_home_page(self):
         driver = self.driver
         driver.get("http://localhost:8080/addressbook/index.php")
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,24 +2,16 @@ from model.group import Group
 
 
 def test_modify_group_name(app):
-    app.session.login(user="admin", pwd="secret")
     app.group.modify_first_group(Group(groupName="updated Name"))
-    app.session.logout()
 
 
 def test_modify_group_header(app):
-    app.session.login(user="admin", pwd="secret")
     app.group.modify_first_group(Group(headerDescr="updated header description"))
-    app.session.logout()
 
 
 def test_modify_group_footer(app):
-    app.session.login(user="admin", pwd="secret")
     app.group.modify_first_group(Group(footerDescr="updated footer description"))
-    app.session.logout()
 
 
 def test_modify_group_emptyGroupName(app):
-    app.session.login(user="admin", pwd="secret")
     app.group.modify_first_group(Group(groupName=""))
-    app.session.logout()
