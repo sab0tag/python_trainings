5e220edb206983cdac27ce2e3a01dce42946f39b
Ihor Petrenko <sabotag1985@gmail.com>
2020-07-26 12:26:05 +0300

    оптимизация формы модификации груп; в fixture/group выделены вспомогательные методы; реазилованы проверки полей ввода на основе данных из тестовых методов(конструкция if/then/else)

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,8 +3,12 @@ from model.group import Group
 
 
 def test_add_new_group(app):
+    app.session.login(user="admin", pwd="secret")
     app.group.create(Group(groupName="newGroup", headerDescr="header description", footerDescr="footer description"))
+    app.session.logout()
 
 
 def test_empty_group(app):
+    app.session.login(user="admin", pwd="secret")
     app.group.create(Group(groupName="", headerDescr="", footerDescr=""))
+    app.session.logout()
