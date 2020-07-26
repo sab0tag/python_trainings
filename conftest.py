8586f6d055e17d27bd6d694f9c4dd7c4065612bb
Ihor Petrenko <sabotag1985@gmail.com>
2020-07-26 15:22:27 +0300

    Оптимизация кода, реализующего login/logout процедуру
    conftest.py
    В методе, инициализирующем фикстуру реализован вспомогательный метод, который проверяет наличие авторизованой сессии для пользователя
    fixture/session.py
    ensure_login - метод, который реализует проверку статуса текущей сессии авторизации: если пользователь не залогинен - логинимся, если залогинен, но несовпадает имя, то разлогинить и зайти снова
    ensure_logout - метод, который реализует проверк

--- conftest.py
+++ conftest.py
@@ -10,17 +10,16 @@ def app(request):
     global fixture
     if fixture is None:
         fixture = Application()
-        fixture.session.login(user="admin", pwd="secret")
     else:
         if not fixture.is_valid():
             fixture = Application()
-            fixture.session.login(user="admin", pwd="secret")
+    fixture.session.ensure_login(user="admin", pwd="secret")
     return fixture
 
 @pytest.fixture(scope="session", autouse=True)
 def stop(request):
     def fin():
-        fixture.session.logout()
+        fixture.session.ensure_logout()
         fixture.destroy()
     # destroy fixture
     request.addfinalizer(fin)
--- fixture/session.py
+++ fixture/session.py
@@ -5,7 +5,7 @@ class SessionHelper:
 
     def login(self, user, pwd):
         driver = self.app.driver
-        self.app.open_home_page() # method
+        self.app.open_home_page()  # method
         driver.find_element_by_name("user").click()
         driver.find_element_by_name("user").clear()
         driver.find_element_by_name("user").send_keys(user)
@@ -16,3 +16,25 @@ class SessionHelper:
     def logout(self):
         driver = self.app.driver
         driver.find_element_by_link_text("Logout").click()
+
+    def ensure_logout(self):
+        driver = self.app.driver
+        if self.is_logged_in():
+            self.logout()
+
+    def ensure_login(self, user, pwd):
+        driver = self.app.driver
+        if self.is_logged_in():
+            if self.is_logged_in_as(user):
+                return
+            else:
+                self.logout()
+        self.login(user, pwd)
+
+    def is_logged_in(self):
+        driver = self.app.driver
+        return len(driver.find_elements_by_link_text("Logout")) > 0
+
+    def is_logged_in_as(self, user):
+        driver = self.app.driver
+        return driver.find_element_by_xpath("//div/div[1]/form/b").text == "("+user+")"
