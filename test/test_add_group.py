2d6eddd195123c8dab0ef818afe98c50a02a4189
Ihor Petrenko <sabotag1985@gmail.com>
2020-08-17 15:22:28 +0300

    Separate test data from the testing scenarios to another package

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,21 +1,7 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 import pytest
-import random
-import string
-
-
-# gen random testdata
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.ascii_lowercase + string.ascii_uppercase + " " * 10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [Group(groupName="", headerDescr="", footerDescr="")] + [
-    Group(groupName=random_string("name", 10), headerDescr=random_string("header", 20),
-          footerDescr=random_string("footer", 20))
-    for i in range(5)
-]
+from data.add_group import constant as testdata
 
 """
 testdata = Group(groupName=name, headerDescr=header, footerDescr=footer)
