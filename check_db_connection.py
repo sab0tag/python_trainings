bb6e7792b52bd019abf8022d904a6c6b6da08fbf
Ihor Petrenko <sabotag1985@gmail.com>
2020-08-27 10:01:45 +0300

    implemented object relationships
    Initial commit

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,14 +1,18 @@
 import pymysql
 #from fixture.db import dbfixture_
 from fixture.orm import ORMFixture
+from model.group import Group
 
 #db = dbfixture_(host="localhost",name="addressbook",user="root",password="")
 db = ORMFixture(host="localhost", name="addressbook", user="root", password="")
 
 try:
     # get db grouplist
-    contacts = db.get_contact_list()
+    # contacts = db.get_contact_list()
     # contacts = db.get_group_list()
+    # contacts = db.get_contacts_in_group(Group(id="12"))
+    contacts = db.get_contacts_not_in_group(Group(id="12"))
+
     for contact in contacts:
         print(contact)
     print(len(contacts))
--- fixture/orm.py
+++ fixture/orm.py
@@ -2,6 +2,8 @@ from pony.orm import *
 from datetime import datetime
 from model.group import Group
 from model.usr import User
+
+
 # from pymysql.converters import decoders
 
 
@@ -15,6 +17,7 @@ class ORMFixture:
         name = Optional(str, column='group_name')
         header = Optional(str, column='group_header')
         footer = Optional(str, column='group_footer')
+        contacts = Set(lambda: ORMFixture.ORMContact, table='address_in_groups', column='id', reverse='groups', lazy=True)
 
     class ORMContact(db.Entity):
         _table_ = 'addressbook'
@@ -22,11 +25,12 @@ class ORMFixture:
         firstname = Optional(str, column='firstname')
         lastname = Optional(str, column='lastname')
         deprecated = Optional(datetime, column='deprecated')
+        groups = Set(lambda: ORMFixture.ORMGroup, table='address_in_groups', column='group_id', reverse='contacts', lazy=True)
 
     def __init__(self, host, name, user, password):
-        self.db.bind('mysql', host=host, database=name, user=user, passwd=password) #conv=decoders)
+        self.db.bind('mysql', host=host, database=name, user=user, passwd=password)  # conv=decoders)
         self.db.generate_mapping()  # class to match class properties with db table fields
-        # sql_debug(True)
+        sql_debug(True)
 
     def cnvrt_groupsToModel(self, groups):
         def convert(group):
@@ -40,9 +44,21 @@ class ORMFixture:
     def cnvrt_contactsToModel(self, contacts):
         def convert(contact):
             return User(id=str(contact.id), name=contact.firstname, surname=contact.lastname)
+
         return list(map(convert, contacts))
 
     @db_session
     def get_contact_list(self):
         # with db_session: # code should be executed during the session
         return self.cnvrt_contactsToModel(select(c for c in ORMFixture.ORMContact if c.deprecated is None))
+
+    @db_session
+    def get_contacts_in_group(self, group):
+        orm_group = list(select(g for g in ORMFixture.ORMGroup if g.id == group.id))[0]
+        return self.cnvrt_contactsToModel(orm_group.contacts)
+
+    @db_session
+    def get_contacts_not_in_group(self, group):
+        orm_group = list(select(g for g in ORMFixture.ORMGroup if g.id == group.id))[0]
+        return self.cnvrt_contactsToModel(
+            select(c for c in ORMFixture.ORMContact if c.deprecated is None and orm_group not in c.groups))
